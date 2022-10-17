8b6c0350e6a90c1e0a52d2c15b8ef33ad6cc3d6c
jasseeeem <jaseemseemse@gmail.com>
2022-10-17 21:21:29 +0300

    added python http.server

--- ddos.py
+++ ddos.py
@@ -11,7 +11,7 @@ def main():
 
     net.addController('c0', ip="127.0.0.1", port=6653)
     
-    # Create the network hosts, each having 10% of the system’s CPU
+    # Create the network hosts, each having 5% of the system’s CPU
     h1 = net.addHost('h1', ip="10.0.0.1", cpu = 0.05)
     h2 = net.addHost('h2', ip="10.0.0.2", cpu=0.05)
     # h3 = net.addHost('h3', ip="10.0.0.3", cpu=0.05)
@@ -40,22 +40,24 @@ def main():
 
     net.addLink(s1, s2)
     net.addLink(s2, s3)
-    net.addLink(s3, s1)
+
+    # Mininet does not support ethernet loops (https://github.com/mininet/mininet/wiki/FAQ#ethernet-loops)
+    # net.addLink(s3, s1)
 
     net.start()
 
-    h5 = net.get('h5')
+    # h5 = net.get('h5')
 
-    h9 = net.get('h9')
-    p1 = h9.popen('python3 myServer.py -i %s &' % h9.IP())
+    # h9 = net.get('h9')
+    # p1 = h9.popen('python3 myServer.py -i %s &' % h9.IP())
 
-    h2 = net.get('h2')
-    h2.cmd('python3 myClient.py -i %s -m "hello world"' % h1.IP())
+    # h2 = net.get('h2')
+    # h2.cmd('python3 myClient.py -i %s -m "hello world"' % h1.IP())
     
     CLI(net, script="script.sh")
     CLI(net)
 
-    p1.terminate()
+    # p1.terminate()
     net.stop()
 
 if __name__ == '__main__':
--- script.sh
+++ script.sh
@@ -1,4 +1,19 @@
-sh ovs-ofctl add-flow s2 priority=800,ip,nw_src=10.0.0.5,actions=normal
-sh ovs-ofctl add-flow s2 priority=500,dl_type=0x800,nw_proto=6,tp_dst=80,actions=output:1
-sh ovs-ofctl add-flow s2 arp,actions=normal
-xterm h5
+# For starting the server on h9
+xterm h9
+# python3 -m http.server 80
+
+# For pinging from h1 to h2 and vice-versa
+sh ovs-ofctl add-flow s1 priority=8000,ip,nw_dst=10.0.0.2,actions=output:2
+sh ovs-ofctl add-flow s1 priority=8000,ip,nw_dst=10.0.0.1,actions=output:1
+
+# For sending request to server in h9 from h5
+sh ovs-ofctl add-flow s2 priority=8000,ip,nw_dst=10.0.0.9,actions=output:4
+sh ovs-ofctl add-flow s2 priority=8000,ip,nw_dst=10.0.0.5,actions=output:1
+
+# sh ovs-ofctl add-flow s2 priority=500,dl_type=0x800,nw_proto=6,tp_dst=80,actions=output:1
+# sh ovs-ofctl add-flow s2 priority=800,ip,nw_src=10.0.0.5,actions=normal
+# sh ovs-ofctl add-flow s2 arp,actions=normal
+
+# For forwarding from s3 to h9
+sh ovs-ofctl add-flow s3 priority=8000,ip,nw_dst=10.0.0.9,actions=output:1
+sh ovs-ofctl add-flow s3 priority=8000,ip,nw_dst=10.0.0.5,actions=output:2

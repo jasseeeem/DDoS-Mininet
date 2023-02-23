31e0cb352bc570ca9394db4fd5509d17e2b0d02d
jasseeeem <jaseemseemse@gmail.com>
2023-02-23 15:19:08 +0300

    hop count check function base code

---
+++ final/hcf.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <net/ethernet.h>
+
+#include "hdf5.c"
+#include "murmur_hash.c"
+
+#define HLIM_BYTE_POSITION 21
+
+int hlim_to_hop_count(int);
+void ProcessPacket(unsigned char *, int);
+
+int sock_raw;
+int i, j;
+struct sockaddr_in6 source, dest;
+
+int hlim_to_hop_count(int hlim)
+{
+    if (hlim > 255 || hlim < 0)
+        return -1;
+    else if (hlim <= 64)
+        return 64 - hlim;
+    else if (hlim <= 128)
+        return 128 - hlim;
+    else
+        return 255 - hlim;
+}
+
+void ProcessPacket(unsigned char *buffer, int size)
+{
+    int i, hlim;
+    char hlim_str[3] = {0}, src_ip[40];
+    struct in6_addr ip_addr;
+    uint32_t hash;
+    char hash_left_str[10], hash_right_str[10];
+
+    snprintf(hlim_str, sizeof(hlim_str), "%02x", buffer[HLIM_BYTE_POSITION]);
+    hlim = (int)strtol(hlim_str, NULL, 16);
+
+    snprintf(src_ip,
+             40,
+             "%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x",
+             buffer[HLIM_BYTE_POSITION + 1],
+             buffer[HLIM_BYTE_POSITION + 2],
+             buffer[HLIM_BYTE_POSITION + 3],
+             buffer[HLIM_BYTE_POSITION + 4],
+             buffer[HLIM_BYTE_POSITION + 5],
+             buffer[HLIM_BYTE_POSITION + 6],
+             buffer[HLIM_BYTE_POSITION + 7],
+             buffer[HLIM_BYTE_POSITION + 8],
+             buffer[HLIM_BYTE_POSITION + 9],
+             buffer[HLIM_BYTE_POSITION + 10],
+             buffer[HLIM_BYTE_POSITION + 11],
+             buffer[HLIM_BYTE_POSITION + 12],
+             buffer[HLIM_BYTE_POSITION + 13],
+             buffer[HLIM_BYTE_POSITION + 14],
+             buffer[HLIM_BYTE_POSITION + 15],
+             buffer[HLIM_BYTE_POSITION + 16]);
+    printf("Source Address: %s\n", src_ip);
+
+    inet_pton(AF_INET6, src_ip, &ip_addr);
+    hash = murmur_hash((const char *)&ip_addr, sizeof(ip_addr), 0);
+    hash = (hash >> 8) & 0xffffff; // Take the least significant 24 bits
+
+    // printf("Hash value: %06x\n", hash);
+
+    // new stuff
+    uint32_t hash_left, hash_right;
+    hash_left = (hash >> 12);
+    // printf("Hash left: %02x\n", hash_left);
+
+    hash_right = (hash & 0xfff);
+    // printf("Hash right: %02x\n", hash_right);
+
+    sprintf(hash_left_str, "%02x", hash_left);
+    sprintf(hash_right_str, "%02x", hash_right);
+    // printf("Hash string: %s\n", hexstr);
+
+    int hash_left_int, hash_right_int;
+    hash_left_int = (int)strtol(hash_left_str, NULL, 16);
+    // printf("Hash left int: %d\n", hash_left_int);
+    hash_right_int = (int)strtol(hash_right_str, NULL, 16);
+    // printf("Hash right int: %d\n", hash_right_int);
+
+    if (check_hop_count(src_ip, hash_left_int, hash_right_int, hlim_to_hop_count(hlim)) == 0)
+    {
+        printf("Hop Count Checked\n");
+    }
+}
+
+int main()
+{
+    int data_size;
+    struct sockaddr_in6 saddr;
+    socklen_t saddr_size;
+    struct in_addr in;
+
+    get_or_create_table();
+    printf("Starting...\n");
+    sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IPV6));
+    if (sock_raw < 0)
+    {
+        printf("Socket Error\n");
+        return 1;
+    }
+
+    unsigned char *buffer = (unsigned char *)malloc(65536);
+    while (1)
+    {
+        saddr_size = sizeof(saddr);
+        // Receive a packet
+        data_size = recvfrom(sock_raw, buffer, 65536, 0, (struct sockaddr *)&saddr, &saddr_size);
+        if (data_size < 0)
+        {
+            printf("Recvfrom error , failed to get packets\n");
+            return 1;
+        }
+        // Now process the packet
+        ProcessPacket(buffer, data_size);
+
+        // NEED A WAY TO TERMINATE THE LOOP
+    }
+    close(sock_raw);
+    printf("Finished");
+    return 0;
+}
---
+++ final/hdf5.c
@@ -0,0 +1,82 @@
+#include <stddef.h>
+#include <hdf5.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE "hdf5.h5"
+#define DATASETNAME "IntArray"
+#define NX 4096
+#define NY 4096
+#define RANK 2
+#define ALLOWED_HOP_COUNT_DIFFERENCE 2
+#define INITIAL_HOP_COUNT -1
+
+int check_hop_count(char src_ip[], int row, int col, int calculated_hop_count)
+{
+    hid_t file, dataset;
+    hid_t datatype, dataspace;
+    herr_t status;
+
+    int *data;
+    /* dynamically allocating memory to the matrix*/
+    data = (int *)malloc(NX * NY * sizeof(int));
+
+    file = H5Fopen(FILE, H5F_ACC_RDWR, H5P_DEFAULT);
+    dataset = H5Dopen2(file, DATASETNAME, H5P_DEFAULT);
+    status = H5Dread(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+    printf("[%d,%d] = %d -> %d\n", row, col, data[row * 4096 + col], calculated_hop_count);
+
+    if (data[row * 4096 + col] == INITIAL_HOP_COUNT)
+    {
+        // do a curl and get the value
+    }
+    else if (data[row * 4096 + col] - calculated_hop_count <= ALLOWED_HOP_COUNT_DIFFERENCE)
+    {
+        printf("✅ Hop Count within the allowed limit\n");
+    }
+    else
+    {
+        printf("❌ Hop Count does not match - Add drop rule");
+    }
+    status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+    H5Dclose(dataset);
+    H5Fclose(file);
+    free(data);
+    return 0;
+}
+
+void get_or_create_table()
+{
+    hid_t file, dataset;       /* file and dataset handles */
+    hid_t datatype, dataspace; /* handles */
+    hsize_t dimsf[2];          /* dataset dimensions */
+    herr_t status;
+
+    int *data; /* data to write */
+    int i, j;
+
+    data = (int *)malloc(16777216 * sizeof(int));
+    memset(data, INITIAL_HOP_COUNT, NX * NY * sizeof(int));
+    file = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+    dimsf[0] = NX;
+    dimsf[1] = NY;
+    dataspace = H5Screate_simple(RANK, dimsf, NULL);
+
+    datatype = H5Tcopy(H5T_NATIVE_INT);
+    status = H5Tset_order(datatype, H5T_ORDER_LE);
+
+    dataset = H5Dcreate(file, DATASETNAME, datatype, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+
+    H5Sclose(dataspace);
+    H5Tclose(datatype);
+    H5Dclose(dataset);
+    H5Fclose(file);
+}
+
+// int main()
+// {
+//     get_or_create_table();
+// }

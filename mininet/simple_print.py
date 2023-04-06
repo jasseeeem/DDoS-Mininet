ad8fce2623848a8082d0188f0fb6adf5a15c7809
jasseeeem <jaseemseemse@gmail.com>
2023-04-06 23:17:57 +0300

    added initial packet rate tracking

--- mininet/simple_print.py
+++ mininet/simple_print.py
@@ -1,17 +1,17 @@
-from ryu.app import simple_switch_13
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib.packet import ethernet
 from ryu.ofproto import ofproto_v1_3, ether
-from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route, Response
 import json
 import requests
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import packet, ethernet, ether_types
 from scapy.all import *
+from scapy.layers.l2 import Ether
+from scapy.layers.inet6 import IPv6
+from scapy.layers.inet import IP
 
 
 class PacketRateMonitor(app_manager.RyuApp):
@@ -61,7 +61,6 @@ class PacketRateMonitor(app_manager.RyuApp):
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
-        print("IT WORKSS")
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -71,30 +70,26 @@ class PacketRateMonitor(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
 
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        # if pkt.haslayer(IP):
-        #     src_ip = pkt[IP].src
-        #     dst_ip = pkt[IP].dst
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
+        eth = Ether(msg.data)
+        
+        if eth.type == ether_types.ETH_TYPE_LLDP:
+            # ignore LLDP packet
             return
-        dst = eth.dst
-        src = eth.src
+
+        dst_mac = eth.dst
+        src_mac = eth.src
 
         dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-        # self.logger.info("packet in %s %s %s %s %s %s", dpid, src, dst, src_ip, dst_ip, in_port)
+        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.logger.info("packet in %s %s %s %s", dpid, src_mac, dst_mac, in_port)
 
         # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        self.mac_to_port[dpid][src_mac] = in_port
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+        if dst_mac in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst_mac]
         else:
             out_port = ofproto.OFPP_FLOOD
 
@@ -102,7 +97,7 @@ class PacketRateMonitor(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac, eth_src=src_mac)
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
@@ -118,26 +113,28 @@ class PacketRateMonitor(app_manager.RyuApp):
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
 
+    # triggered when switch sends EventOFPSwitchFeatures message to controller - indicates that switch has connected to controller and is ready to communicate
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-
+        # match all packets
         match = parser.OFPMatch()
+        # send them to the controller
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-        # Enable port statistics
+        # Enable port statistics for the switch
         req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
         # Create an OFPMatch object with no match criteria
         match = parser.OFPMatch()
 
-        # Enable aggregate statistics
+        # Enable aggregate statistics to retrieve statistics about the entire switch, such as the number of packets and bytes processed by the switch
         req = parser.OFPAggregateStatsRequest(datapath, 0, cookie=0, cookie_mask=0, match=match, table_id=ofproto.OFPTT_ALL, out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY)
         datapath.send_msg(req)
 
@@ -146,12 +143,15 @@ class PacketRateMonitor(app_manager.RyuApp):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        # get the body of the port statistics reply message
         stats = ev.msg.body
 
-        # Compute packet rates for each port
+        # loop over each port in the statistics
         for stat in stats:
             port_no = stat.port_no
+            # number of received packets
             rx_packets = stat.rx_packets
+            # number of transmitted packets
             tx_packets = stat.tx_packets
             duration_sec = stat.duration_sec
             duration_nsec = stat.duration_nsec
@@ -163,22 +163,35 @@ class PacketRateMonitor(app_manager.RyuApp):
 
     def send_packet_rate_data(self, data):
         # Send the packet rate data to the Ryu REST API server
-        url = 'http://127.0.0.1:8080/packet_rate_monitor/packet_rate'
+        url = 'http://127.0.0.1:8080/packet_rate'
         response = requests.post(url, json=data)
         if response.status_code != 200:
             self.logger.error('Failed to send packet rate data')
 
+# REST API controller class
 class PacketRateMonitorController(ControllerBase):
+    # store as class attribute so that it is not reset after every API call (which is what happens if put inside __init__)
+    packet_rate_data = {}
+
+    # When a new HTTP request is received, the web framework instantiates a new instance of the controller class to handle the request. 
+    # The __init__ method is called during the instantiation process to initialize the attributes of the new instance. 
+    # The attributes of the previous instances are not preserved since they are specific to that instance and will be destroyed when the instance is no longer needed.
     def __init__(self, req, link, data, **config):
         super(PacketRateMonitorController, self).__init__(req, link, data, **config)
-        self.packet_rate_data = data
+        # self.packet_rate_data = data 
 
+    # update the packet rate of a switch
     @route('packet_rate_monitor', '/packet_rate', methods=['POST'])
     def packet_rate(self, req, **kwargs):
         data = req.json
-        self.packet_rate_data[data['switch_id']] = data
+        print(PacketRateMonitorController.packet_rate_data)
+        if data['switch_id'] in PacketRateMonitorController.packet_rate_data.keys():
+            PacketRateMonitorController.packet_rate_data[data['switch_id']][data['port_no']] = data['packet_rate']
+        else:
+            PacketRateMonitorController.packet_rate_data[data['switch_id']] = {data['port_no']: data['packet_rate']}
         return Response(status=200)
 
+    # get the packet rates of all switches
     @route('packet_rate_monitor', '/packet_rate', methods=['GET'])
     def get_packet_rate(self, req, **kwargs):
-        return json.dumps(self.packet_rate_data)
+        return json.dumps(PacketRateMonitorController.packet_rate_data)
